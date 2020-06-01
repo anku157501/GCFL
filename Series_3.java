ff6746df8c897bd4439d59e1f17f849ad2acaba7
Ankita Kushwaha <46566985+anku157501@users.noreply.github.com>
2020-06-02 00:39:53 +0530

    Create Series_3.java

---
+++ Series_3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+          Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=0;
+        if(n!=1){
+        for(int i=1;i<n;i++){
+        a=a+2*((2*i)-1)+5;
+            
+        }}
+            System.out.println(a);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
